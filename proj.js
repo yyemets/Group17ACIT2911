fc4c310e90c4ec9f3ccbc2cdf3824c58169c03e9
Ryan Cho <ryancho.hakhyeon@gmail.com>
2020-05-01 08:53:40 -0700

    Update proj.js

--- proj.js
+++ proj.js
@@ -5,9 +5,25 @@ let score = 0;
 let highscore = 0;
 let timer;
 let timeout;
-
+let gameTimer;
+let time = 30;
+let bgMusic = document.getElementById("bgmusic");
+        
 function letsRandomize(min,max){
      return (Math.floor(Math.random() * max) + min);
+}
+
+function lowerVolume(){
+    bgMusic.volume = 0.2;
+}
+
+function bgPlay(){
+    bgMusic.play()
+}
+
+function bgStop(){
+    bgMusic.pause();
+    bgMusic.currentTime = 0;
 }
 
 function zombiePlacer(){
@@ -23,6 +39,19 @@ function zombiePlacer(){
         showZombie(4);
         zombieMove(4,120);
     }
+}
+
+function countdown() {
+    let elem = document.getElementById('timer');
+    if (time == 1) {
+        reset();
+        console.log('timer done');
+        elem.innerHTML = 'Time: ' + time;
+        
+    } else {
+        time--;
+        elem.innerHTML = 'Time: ' + time;
+  }
 }
 
 function zombieMove(id,s){
@@ -65,28 +94,28 @@ function initialize(){
     let sh = window.innerHeight/2;
     count = 0;
     score = 0;
+    time = 30;
     zombieMove(1,200);
     document.getElementById('start').style.top = sh - 40 + 'px';
-    document.getElementById('start').style.left = sw - 40 + 'px';
-    document.getElementById('reset').style.top = window.innerHeight - 40 + 'px';
-    document.getElementById('reset').style.left = sw - 40 + 'px';
+    document.getElementById('start').style.left = sw - 60 + 'px';
     document.getElementById('score').innerHTML = "Score: " + score;
     document.getElementById('hscore').innerHTML = "High Score: " + highscore;
+    document.getElementById('timer').innerHTML = "Time: " + time;
     document.getElementById('head').innerHTML = "Whack-A-Mole";
     document.getElementById('start').style.display = "inline-block";
     document.getElementById('mole1').onclick = handler(1, 200);
     document.getElementById('mole2').onclick = handler(2, 200);
     document.getElementById('mole3').onclick = handler(3, 240);
     document.getElementById('mole4').onclick = handler(4, 120);
-    document.getElementById('reset').style.display = "none";
     document.getElementById('start').onclick = gameStart;
-    document.getElementById('reset').onclick = reset;
     hideAll();
 }
 function reset(){
     initialize();
     stopTimer();
     clearTimeout(timeout);
+    clearInterval(gameTimer);
+    bgStop();
 }
         
 function hideAll(){
@@ -98,16 +127,16 @@ function hideAll(){
     document.getElementById('boom2').style.display = "none";
     document.getElementById('boom3').style.display = "none";
     document.getElementById('boom4').style.display = "none";
-    document.getElementById('reset').style.display = "none";
 }
         
 function gameStart(){
     document.getElementById('start').style.display = "none";
-    document.getElementById('reset').style.display = "inline-block";
     document.getElementById('head').innerHTML = "";
     zombieMove(1,200);
     startTimer();
+    gameTimer = setInterval(countdown, 1000);
     showZombie(1);
+    bgPlay();
 }
 
 function startTimer(){
@@ -123,6 +152,10 @@ function handler(id, s){
         document.getElementById('effect' + id).play();
         score += 1;
         animate(id);
+        if (score % 2 == 0) {
+            time += 1;
+        }
+        document.getElementById('timer').innerHTML = 'Time: ' + time;
         moveClicked(id, s);
         document.getElementById('score').innerHTML = "Score: " + score;
         if (score > highscore){
