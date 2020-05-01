df81be4620efe9536804bc2c6922c0b313a6f73e
Ryan Cho <ryancho.hakhyeon@gmail.com>
2020-05-01 00:31:23 -0700

    test

---
+++ test/test.js
@@ -0,0 +1,10 @@
+var myCode = require('../proj')
+var assert = require('chai');
+
+describe('Test', function(){
+    describe('#initialize()', function(){
+        it('should return 0 initial value', function(){
+          assert.equal()
+        })
+    })
+})
