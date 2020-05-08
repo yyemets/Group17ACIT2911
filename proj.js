0c7ac659b10330520a0c07147a9f69bb9c2c3530
Ryan Cho <ryancho.hakhyeon@gmail.com>
2020-05-08 08:23:58 -0700

    modify

--- proj.js
+++ proj.js
@@ -5,6 +5,7 @@ let score = 0;
 let highscore = 0;
 let scoreboard = [];
 let speed = 2000;
+
 //Movement Intervals
 let timer1;
 let timer2;
@@ -22,13 +23,12 @@ let four = false;
 
 let gameTimer;
 let time = 0;
-let bgMusic = document.getElementById("bgmusic");
 
 let survival = false;
 let insane = false;
 let lives = 10;
  
-
+let bgMusic = document.getElementById("bgmusic");
 // scoreboard.sort(function(a, b){return b - a});
 
 // Randomizer
@@ -129,22 +129,20 @@ function countdown() {
     } else {
         time++;
         elem.innerHTML = 'Time: ' + time;
-        if (time % 10 == 0) {
-            speed -= 200;
-        }
-    } 
+    }
+
     
-    if(score >= 5 && !(two)) {
+    if((score >= 5 && !(two)) || (insane && !(two))) {
         two = true;
         showMole(2);
         timer2 = setTimeout(molePlacer2, speed);
     }
-    if(score == 10 && !(three)) {
-        tnree = true;
+    if((score == 10 && !(three)) || (insane && !(three))) {
+        three = true;
         showMole(3);
         timer3 = setTimeout(molePlacer3, speed);
     }
-    if(score == 20 && !(four)){
+    if((score == 20 && !(four)) || (insane && !(four))){
         four = true;
         showMole(4);
         timer4 = setTimeout(molePlacer4, speed);
@@ -280,26 +278,33 @@ function initialize(){
     four = false;
     moleMove(1,200);
     survival = false;
+    insane = false;
     document.getElementById('survival').style.backgroundColor = "#331a00";
     document.getElementById('survival').style.color = "white";
+    document.getElementById('insane').style.backgroundColor = "#331a00";
+    document.getElementById('insane').style.color = "white";
     document.getElementById('start').style.top = sh - 40 + 'px';
     document.getElementById('start').style.left = sw - 60 + 'px';
     document.getElementById('head').style.top = sh - 400 + 'px';
     document.getElementById('head').style.left = sw - 270 + 'px';
-    document.getElementById('survival').style.top = sh + 20 + 'px';
+    document.getElementById('survival').style.top = sh + 80 + 'px';
     document.getElementById('survival').style.left = sw - 73 + 'px';
+    document.getElementById('insane').style.top = sh + 20 + 'px';
+    document.getElementById('insane').style.left = sw - 65 + 'px';
     document.getElementById('score').innerHTML = "Score: " + score;
     document.getElementById('hscore').innerHTML = "High Score: " + highscore;
     document.getElementById('timer').innerHTML = "Time: " + time;
     document.getElementById('head').innerHTML = "Whack-A-Mole";
     document.getElementById('start').style.display = "inline-block";
     document.getElementById('survival').style.display = "inline-block";
+    document.getElementById('insane').style.display = "inline-block";
     document.getElementById('mole1').onclick = handler(1, 200);
     document.getElementById('mole2').onclick = handler(2, 200);
     document.getElementById('mole3').onclick = handler(3, 240);
     document.getElementById('mole4').onclick = handler(4, 120);
     document.getElementById('start').onclick = gameStart;
     document.getElementById('survival').onclick = survivalMode;
+    document.getElementById('insane').onclick = insaneMode;
     document.getElementById('lives').style.display = "none";
     
     hideAll();
@@ -337,6 +342,7 @@ function gameStart(){
         time = 30;
     }
     document.getElementById('survival').style.display = "none";
+    document.getElementById('insane').style.display = "none";
     document.getElementById('timer').innerHTML = "Time: " + time;
     document.getElementById('start').style.display = "none";
     document.getElementById('head').innerHTML = "";
@@ -355,12 +361,40 @@ function survivalMode() {
         document.getElementById('survival').style.backgroundColor = "white";
         document.getElementById('survival').style.color = "black";
         document.getElementById('lives').innerHTML = "Lives: " + lives;
+        if (insane == true) {
+            insane = false;
+            speed += 500;
+            document.getElementById('insane').style.backgroundColor = "#331a00";
+            document.getElementById('insane').style.color = "white";
+        }
     } else {
         survival = false;
         console.log('off');
         document.getElementById('survival').style.backgroundColor = "#331a00";
         document.getElementById('survival').style.color = "white";
         document.getElementById('lives').style.display = "none";
+    }
+    
+}
+
+function insaneMode() {
+    if (insane == false) {
+        insane = true;
+        speed -= 500;
+        document.getElementById('insane').style.backgroundColor = "white";
+        document.getElementById('insane').style.color = "black";
+        if (survival == true) {
+            survival = false;
+            console.log('off');
+            document.getElementById('survival').style.backgroundColor = "#331a00";
+            document.getElementById('survival').style.color = "white";
+            document.getElementById('lives').style.display = "none";
+        }
+    } else {
+        insane = false;
+        speed += 500;
+        document.getElementById('insane').style.backgroundColor = "#331a00";
+        document.getElementById('insane').style.color = "white";
     }
     
 }
@@ -382,6 +416,16 @@ function handler(id, s){
         document.getElementById('effect' + id).play();
         score += 1;
         animate(id);
+        if (insane || survival) {
+            if (score % 10 == 0) {
+                if ((speed - 100) > 200) {
+                    speed -= 100;
+                    console.log(speed)
+                } else {
+                    speed = 200;
+                }
+            }
+        }
         if (survival == false){
             if (score % 2 == 0) {
                 time += 1;
@@ -412,12 +456,11 @@ function handler(id, s){
     }
 }
 
-//document.body.onload = initialize;
+document.body.onload = initialize;
 
-//module.exports = { count, score, highscore, time, timer, 
-//    timeout, gameTimer, bgMusic, letsRandomize }; 
-
-module.exports = { count, score, highscore, time, timer, 
-    timeout, gameTimer, letsRandomize };
+module.exports = { count, score, highscore, speed, scoreboard, survival, insane, lives,
+    time, gameTimer, timer1, timer2, timer3, timer4, timeout1, timeout2, timeout3, timeout4, 
+    two, three, four,
+    moleMove, reset, letsRandomize };
 
 
--- test/test.js
+++ test/test.js
@@ -1,60 +1,132 @@
 const expect = require('chai').expect;
-const myCode = require('../proj.js');
+const jsdom = require('mocha-jsdom');
+
+global.document = jsdom({url: "http://localhost"});
 
 describe('Test', function(){    
   
-  describe('# Initial Values', function(){
-    it('zero initial count value', function(){
+  describe('# Initial Basic Values', function(){
+    it('Initial count value 0', function(){
+      var myCode = require('../proj.js');
       expect(myCode.count).to.equal(0);
     });
-    it('Number initial count type', function(){
-      expect(myCode.count).to.be.a('number');
-    });
-
-    it('zero initial score value', function(){
+    it('Initial score value 0', function(){
+      var myCode = require('../proj.js');
       expect(myCode.score).to.equal(0);
     });
-    it('Number initial score type', function(){
-      expect(myCode.score).to.be.a('number');
-    });
-
-    it('zero initial highscroe value', function(){
+    it('Initial highscroe value 0', function(){
+      var myCode = require('../proj.js');
       expect(myCode.highscore).to.equal(0);
     });
-    it('Number initial highscore type', function(){
+    it('Initial speed value 2000', function(){
+      var myCode = require('../proj.js');
+      expect(myCode.speed).to.equal(2000);
+    });
+
+    it('Initial count value type', function(){
+      var myCode = require('../proj.js');
+      expect(myCode.count).to.be.a('number');
+    });
+    it('Initial score value type', function(){
+      var myCode = require('../proj.js');
+      expect(myCode.score).to.be.a('number');
+    });
+    it('Initial highscore value type', function(){
+      var myCode = require('../proj.js');
       expect(myCode.highscore).to.be.a('number');
     });
-
-    it('30 initial time value', function(){
-      expect(myCode.time).to.equal(30);
+    it('Initial speed value type', function(){
+      var myCode = require('../proj.js');
+      expect(myCode.speed).to.be.a('number');
     });
+
+    it('Initial scoreboard type', function(){
+      var myCode = require('../proj.js');
+      expect(myCode.scoreboard).to.be.a('array');
+    });
+
+    it('Initial survival type', function(){
+      var myCode = require('../proj.js');
+      expect(myCode.survival).to.be.false;
+    });
+    it('Initial insane type', function(){
+      var myCode = require('../proj.js');
+      expect(myCode.insane).to.be.false;
+    });
+    it('Initial lives value 10', function(){
+      var myCode = require('../proj.js');
+      expect(myCode.lives).to.equal(10);
+    });
+    it('Initial lives value type', function(){
+      var myCode = require('../proj.js');
+      expect(myCode.lives).to.be.a('number');
+    });
+
+  });
+
+  describe('# Initial Timer Values', function(){
+    it('initial time value', function(){
+      var myCode = require('../proj.js');
+      expect(myCode.time).to.equal(0);
+    });
+    
     it('Number initial time type', function(){
+      var myCode = require('../proj.js');
       expect(myCode.time).to.be.a('number');
     });
+    
+    it('undefined timer1-4 values', function(){
+      var myCode = require('../proj.js');
+      expect(myCode.timer1).to.be.undefined;
+      expect(myCode.timer2).to.be.undefined;
+      expect(myCode.timer3).to.be.undefined;
+      expect(myCode.timer4).to.be.undefined;
+    });
 
-    it('undefined Timer', function(){
-      expect(myCode.timer).to.be.undefined;
+    it('undefined timeout1-4 values', function(){
+      var myCode = require('../proj.js');
+      expect(myCode.timeout1).to.be.undefined;
+      expect(myCode.timeout2).to.be.undefined;
+      expect(myCode.timeout3).to.be.undefined;
+      expect(myCode.timeout4).to.be.undefined;
     });
-    it('undefined Timeout', function(){
-      expect(myCode.timer).to.be.undefined;
+
+    it('Boolean two, three, four values', function(){
+      var myCode = require('../proj.js');
+      expect(myCode.two).to.be.false;
+      expect(myCode.three).to.be.false;
+      expect(myCode.four).to.be.false;
     });
+
     it('undefined GameTimer', function(){
+      var myCode = require('../proj.js');
       expect(myCode.gameTimer).to.be.undefined;
     });
   });
 
-  describe('# Check Functions', function(){
-    it('letsRandomize() should be a number', function () {
+  describe('# Check Functions', function() {
+    it('letsRandomize() should be a number', function() {
+      var myCode = require('../proj.js');
       let result = myCode.letsRandomize();
       expect(result).to.be.a('number');
     });
 
-    it('zombiePlacer() ');
+    it('molePlacer1-4()');
 
-    it('countdown() ');
+    it('moveClicked1-4()');
 
-    it('showZ()  ');
+    it('reset()' );
 
-    it(' ');
+    it('showM1-4()' );
+
+    it('showMole()');
+
+    it('animate()');
+
+    it('gameStart()');
+
+    it('survivalMode()');
+
+    it('insaneMode()');
   });
 })
